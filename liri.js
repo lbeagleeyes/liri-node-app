51d8e1f5e2b7a80ce79c2fc3c84819ca28e2d212
Laura B <laurabermudezg@gmail.com>
2019-05-02 00:58:10 -0700

    Formatted spotify response, finish functionality, clean up code

--- liri.js
+++ liri.js
@@ -3,6 +3,8 @@ var axios = require("axios");
 var keys = require("./keys.js");
 var fs = require("fs");
 var Spotify = require('node-spotify-api');
+var moment = require('moment');
+
 var spotify = new Spotify({
   id: keys.spotify.id,
   secret: keys.spotify.secret
@@ -11,13 +13,13 @@ var spotify = new Spotify({
 function showMovieInfo(movie) {
   if (movie == "") {
     movie = "Mr.Nobody"
-    console.log("No movie entered, showing results for Mr.Nobody: \n");
+    writeToTxt("No movie entered, showing results for Mr.Nobody: \n");
   }
 
   axios.get(`http://www.omdbapi.com/?t=${movie}&y=&plot=short&apikey=trilogy`).then(
     function (response) {
       //show
-      console.log(`\nMovie Title: ${response.data.Title} \nYear: ${response.data.Year}\nRating: ${response.data.imdbRating}\nRotten Tomatoes Rating: ${response.data.Ratings[1].Value}\nCountry: ${response.data.Country}\nLanguage:${response.data.Language}\nPlot: ${response.data.Plot}\nActors: ${response.data.Actors}\n`);
+      writeToTxt(`\nMovie Title: ${response.data.Title} \nYear: ${response.data.Year}\nRating: ${response.data.imdbRating}\nRotten Tomatoes Rating: ${response.data.Ratings[1].Value}\nCountry: ${response.data.Country}\nLanguage:${response.data.Language}\nPlot: ${response.data.Plot}\nActors: ${response.data.Actors}\n`);
     }
   );
 }
@@ -28,18 +30,17 @@ function showConcerts(artist) {
     axios.get(`https://rest.bandsintown.com/artists/${artist}/events?app_id=codingbootcamp`).then(
       function (response) {
 
-        console.log("\nConcerts: \n");
-       // console.log(response.data);
+        writeToTxt(`\nConcerts for ${artist}: \n`);
 
         var concerts = response.data;
         concerts.forEach(event => {
-          //var eventDate = moment((event.datetime, "MM/DD/YYYY");
-          console.log(` ${event.venue.name} - ${event.venue.city}, ${event.venue.region}\tDate: - ${event.datetime}\n`); 
+          var eventDate = moment(event.datetime).format("MM/DD/YYYY");
+          writeToTxt(`\n${event.venue.name} - ${event.venue.city}, ${event.venue.region}\t\tDate: - ${eventDate}`);
         });
       }
     );
   } else {
-    console.log("\nNo artist was provided \n");
+    writeToTxt("\nNo artist was provided \n");
   }
 }
 
@@ -47,40 +48,61 @@ function showSong(song) {
 
   if (song == "") {
     song = "The Sign";
-    console.log("\nNo song provided, showing results for The Sign by Ace of Base\n");
+    writeToTxt("\nNo song provided, showing results for The Sign by Ace of Base\n");
   }
 
-  spotify.search({ type: 'track', query: song }, function(err, data) {
+  spotify.search({ type: 'track', query: song }, function (err, data) {
     if (err) {
-      return console.log('Error occurred: ' + err);
+      return writeToTxt('Error occurred: ' + err);
     }
-   
-  console.log(JSON.stringify(data)); 
+
+    var tracksFound = data.tracks.items;
+    writeToTxt("\nTracks found:\n");
+    for (var i = 0; i < tracksFound.length; i++) {
+
+      writeToTxt(`\nArtists: ${getArtists(tracksFound[i].artists)}
+                  \nPreview URL: ${tracksFound[i].preview_url != null?tracksFound[i].preview_url: ""}
+                  \nSong Name: ${tracksFound[i].name}
+                  \nAlbum Name: ${tracksFound[i].album.name}\n`);
+
+    }
   });
+}
 
-  //call spotify api - authenticate and then call 
+function getArtists(artistsArr){
+  var artistsList = "";
+  for (var i=0; i<artistsArr.length; i++){
+    artistsList += artistsArr[i].name;
+    if((i+1)<artistsArr.length){
+      artistsList += ", ";
+    }
+  }
+  return artistsList;
+}
 
-  //show:
-  // artist
-  // song's name
-  // preview link
-  // album the song is from
-
+function writeToTxt(str) {
+  console.log(str);
+  fs.appendFile("log.txt", str, function (err) {
+    // If the code experiences any errors it will log the error to the console.
+    if (err) {
+      return console.log(err);
+    }
+  });
 }
 
 function doWhatItSays() {
   //read txt file and call performAction function with instruction and data
-  console.log("Do what it says called");
-  fs.readFile("random.txt", "utf8", function(error, data) {
+  writeToTxt("Do what it says called");
+  fs.readFile("random.txt", "utf8", function (error, data) {
     if (error) {
-      return console.log(error);
+      return writeToTxt(error);
     }
     var dataArr = data.split(",");
-    console.log(`Instruction: ${dataArr[0]}, Data: ${dataArr[1]}`);
+    writeToTxt(`Instruction: ${dataArr[0]}, Data: ${dataArr[1]}`);
     performAction(dataArr[0], dataArr[1]);
-    
+
   });
-  
+
 }
 
 function performAction(instruction, data) {
@@ -98,7 +120,7 @@ function performAction(instruction, data) {
       doWhatItSays();
       break;
     default:
-      console.log("\nInstruction not supported please enter one of the follwing options:\n concert-this \n spotify-this-song \n movie-this \n do-what-it-says \n");
+      writeToTxt("\nInstruction not supported please enter one of the follwing options:\n concert-this \n spotify-this-song \n movie-this \n do-what-it-says \n");
       break;
   }
 }
